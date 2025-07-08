78c7e61eb44c8651a4abfee55fbf76af452d5f20
nekosu <liao.hy@outlook.com>
2025-07-09 03:09:54 +0800

    fix: typo

--- build-libcxx-stage1.sh
+++ build-libcxx-stage1.sh
@@ -14,7 +14,7 @@ echo "Build first stage libc++"
 cmake -G Ninja -S llvm-project/runtimes -B build-libcxx-stage1 \
   -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`pwd`/x-tools/x86_64-linux-gnu/x86_64-linux-gnu/sysroot/usr \
   -DCMAKE_TOOLCHAIN_FILE=`pwd`/clang/stage1.cmake \
-  -DLLVM_ENABLE_RUNTIMES="compiler-rt" \
+  -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind" \
   -DPython3_EXECUTABLE=/usr/bin/python3
 
 cmake --build build-libcxx-stage1 -- -j 16
