2e1b6704bd8111cf07cb9606647019344ea6e069
nekosu <liao.hy@outlook.com>
2025-07-09 15:58:15 +0800

    fix: flag

--- build-clang-stage2.sh
+++ build-clang-stage2.sh
@@ -5,6 +5,7 @@ cmake -G Ninja -S llvm-project/llvm -B build-llvm-stage2 \
   -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=`pwd`/stage2 \
   -DCMAKE_TOOLCHAIN_FILE=`pwd`/clang/stage1-build-stage2.cmake \
   -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" -DLLVM_TARGETS_TO_BUILD="X86;AArch64" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON \
+  -DLLVM_ENABLE_LIBCXX=ON \
   -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON \
   -DCLANG_DEFAULT_LINKER="lld" -DCLANG_DEFAULT_CXX_STDLIB="libc++" -DCLANG_DEFAULT_RTLIB="compiler-rt" -DCLANG_DEFAULT_UNWINDLIB="libunwind" \
   -DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-linux-gnu
