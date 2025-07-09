250c38dca5edc298296a20ce1fae1e5cdc89bb55
nekosu <liao.hy@outlook.com>
2025-07-09 14:40:03 +0800

    feat: disable lto. add tests

--- build-clang-stage2.sh
+++ build-clang-stage2.sh
@@ -7,7 +7,7 @@ cmake -G Ninja -S llvm-project/llvm -B build-llvm-stage2 \
   -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" -DLLVM_TARGETS_TO_BUILD="X86;AArch64" -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_LINK_LLVM_DYLIB=ON \
   -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON \
   -DCLANG_DEFAULT_LINKER="lld" -DCLANG_DEFAULT_CXX_STDLIB="libc++" -DCLANG_DEFAULT_RTLIB="compiler-rt" -DCLANG_DEFAULT_UNWINDLIB="libunwind" \
-  -DLLVM_ENABLE_LTO=Thin -DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-linux-gnu
+  -DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-linux-gnu
 
 cmake --build build-llvm-stage2 -- -j 16
 cmake --install build-llvm-stage2
